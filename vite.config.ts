2ad95ae1328d15f7d2141cb007df8954a5eb0b85
Nogai Artem <mr.yjufq@bk.ru>
2023-09-29 19:37:50 +0600

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "https://Defiler06.github.io/technodom-tester",
+  plugins: [react()],
+})
